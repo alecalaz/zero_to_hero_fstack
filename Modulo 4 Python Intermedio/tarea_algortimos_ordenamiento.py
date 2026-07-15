7fac7c0c562eae5b3429b82a71d867ac6f2c6fa4
Alecalaz <aleca0899@gmail.com>
2026-07-14 18:15:27 -0600

    agregando bubble sort function y lista de preuba 2, se verifico funcionalidad

--- Modulo 4 Python Intermedio/tarea_algortimos_ordenamiento.py
+++ Modulo 4 Python Intermedio/tarea_algortimos_ordenamiento.py
@@ -1,3 +1,18 @@
+def bubble_sort(list_to_sort):
+    for outer_index in range(0, len(list_to_sort)-1):
+        has_made_changes = False
+        for index in range(0, len(list_to_sort)-1 - outer_index):
+            current_element = list_to_sort[index]
+            next_element = list_to_sort[index + 1]
+            if current_element > next_element:
+                list_to_sort[index] = next_element
+                list_to_sort[index + 1] = current_element
+                has_made_changes = True
+        if not has_made_changes:
+            return
+
+
+
 def reverse_bubble_sort(list_to_sort):
     for outer_index in range(0, len(list_to_sort) - 1):
         has_made_changes = False
@@ -16,6 +31,9 @@ def reverse_bubble_sort(list_to_sort):
         if not has_made_changes:
             return
 my_test_list = [18, -11, 68, 6, 32, 53, -23]
+test_list_2 = [18, -11, 68, 6, 32, 53, -23]
 reverse_bubble_sort(my_test_list)
+bubble_sort(test_list_2)
 print(my_test_list)
+print(test_list_2)
 
